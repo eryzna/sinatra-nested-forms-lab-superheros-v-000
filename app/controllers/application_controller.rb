92d378b46ddd76287bbb4057485371c85833a4e8
Elana Ryznar <eryzna@gmail.com>
2019-09-20 22:53:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,15 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
+      erb :super_hero
 
     end
 
     get '/super_hero/new' do
       erb :super_hero
     end
+
+    post '/teams'
 
 
 end
